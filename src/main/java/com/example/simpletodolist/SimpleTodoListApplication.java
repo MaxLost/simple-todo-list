69429ae00361fde225e745b5df53a44f685be5db
Maksim Tisheninov <stepmax14@gmail.com>
2023-03-22 00:19:57 +0300

    Reworked way of deleting tasks after their completion

--- src/main/java/com/example/simpletodolist/SimpleTodoListApplication.java
+++ src/main/java/com/example/simpletodolist/SimpleTodoListApplication.java
@@ -3,9 +3,7 @@ package com.example.simpletodolist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
